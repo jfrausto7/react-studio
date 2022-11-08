13e407b94dfaec45e511895980a884d8c6e30e1e
jfrausto7 <jacob_frausto@brown.edu>
2022-11-07 22:34:28 -0500

    fill in requirements

--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,17 @@
-// TODO: create a component that displays a single bakery item
+import React from "react";
+
+const BakeryItem = ({ name, description, picture, price, addToCart }) => (
+  <div className="BakeryItem">
+    <div className="Image">
+      <img src={picture} alt={name} />
+    </div>
+    <div className="Name">{name + ` ($${price})`}</div>
+    <div className="Description">{description}</div>
+    <div className="Footer">
+      <button className="Button" onClick={addToCart}>
+        Add to Cart
+      </button>
+    </div>
+  </div>
+);
+export default BakeryItem;
